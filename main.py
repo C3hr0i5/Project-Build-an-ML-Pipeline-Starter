f66a755b0ff6b8f8ef90fbd7f9f559c0584ed125
Chris Espin <cespin@aegischemical.com>
2025-10-04 17:27:29 -0500

    syntax

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def go(config: DictConfig):
         f"{config['main']['components_repository']}/train_val_test_split",
         "main",
         parameters={
-            "input": "clean_sample.csv:latest",                 
+            "input": "clean_sample.csv:latest",                
             "artifact_root": "data",
             "test_size":   config["modeling"]["test_size"],
             "random_seed": config["modeling"]["random_seed"],
